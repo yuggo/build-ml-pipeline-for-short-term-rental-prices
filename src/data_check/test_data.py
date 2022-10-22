fb80250bbcb2df8a33f107f9c1038c70f79f16a7
yuggo <denis.vuckovac@gmail.com>
2022-10-21 19:56:48 -0500

    data check

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    """
+    Make sure the data is reasonably big, not too small not too large.
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Make sure the price range is within reasonable bounds
+    """
+    assert data['price'].between(min_price, max_price).all()
